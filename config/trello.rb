d7efec71813719b671c30180cd395262fd106354
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2020-01-14 21:39:15 +0900

    trello の configuration

---
+++ config/trello.rb
@@ -0,0 +1,8 @@
+require 'trello'
+require 'dotenv'
+
+Dotenv.load
+Trello.configure do |config|
+  config.developer_public_key = ENV["TRELLO_DEVELOPER_PUBLIC_KEY"]
+  config.member_token = ENV["TRELLO_MEMBER_TOKEN"]
+end
